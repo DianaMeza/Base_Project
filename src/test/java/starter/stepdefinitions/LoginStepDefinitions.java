dca5bbbb026366438621d95a6776df1de502501f
Diana Meza <diana.meza@realpage.com>
2020-11-19 12:59:37 -0500

    initial compilation successful

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -1,4 +1,52 @@
 package starter.stepdefinitions;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.actions.OpenPage;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import starter.navigation.NavigateTo;
+
+import java.util.List;
+
+import static com.google.common.base.Predicates.equalTo;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+
 public class LoginStepDefinitions {
+
+    String name;
+
+    @Before
+    public void setTheStage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+    @Given("the (.*) is on page WMU")
+    public void the_User_is_on_page_WMU(String user) {
+        this.name = user;
+            theActorCalled(this.name).attemptsTo(
+                    NavigateTo.WmuPage()
+            );
+        }
+
+    @When("enters the credentials")
+    public void enters_the_credentials(List<String> form) {
+
+    }
+
+    @Then("the user is successfully logged in")
+    public void the_user_is_successfully_logged_in() {
+        theActorInTheSpotlight().should(
+
+        );
+
+    }
+
+
+
 }
